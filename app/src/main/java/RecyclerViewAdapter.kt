55936941494113cc3945d92d201dd60a5a4b6f48
Ricky Solorio <rickysolorio@Rickys-MacBook-Pro.local>
2018-10-09 19:07:51 -0700

    Initial commit

---
+++ app/src/main/java/RecyclerViewAdapter.kt
@@ -0,0 +1,5 @@
+class RecyclerViewAdapter extends FragmentManager{ /*make the functions for you code and safe it has a gist for your rerences to code you need your app*/ /*sso you can put it together later!!!!*/
+
+
+
+}
---
+++ app/src/main/java/com/example/rickyjsolorio/myapplication/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.rickyjsolorio.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() /*INCLUDE THE EXTENED OR IMPLEMATAION OF THE  FUNCTION YOU WILL USE FROME THE "LIST VIEW METHODS IMPLEMENTATION " YOU WILL USE IN THE MAIN ACTI*/
+
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+        /*On click listener for Spider List */
+
+
+
+
+       ////////////////////////////////////////////////////////////
+        
+
+
+     /*
+
+
+
+
+
+     */
+
+
+        ///////////////////////////////////////////////////////////
+
+              /*Before you make the next commit if the  previous  fragment needs to be save just incase user need to go back to that screen to the previouse fragment */
+
+
+
+
+
+
+
+
+        //////////////////////////////////////////////////////////
+
+
+
+
+
+
+             /*DON'T FORGRT THE LAST COMMIT COMMIT FOR THE WHOLE THING TO WORK FOR THE METHODS OF */
+
+
+
+
+
+
+
+
+
+
+
+       ///////////////////////////////////////////////////////
+
+
+
+
+
+
+
+    }
+
+
